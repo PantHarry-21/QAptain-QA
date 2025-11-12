6b3099ae9d56d6a1a923a7d45239c3607d2dd8db
Himanshu Pant <himanshupant.qa@gmail.com>
2025-11-12 11:03:24 +0530

    nextauth forced to nodejs

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,8 @@ import { getAuthOptions } from "./../../../../lib/auth"
 export const dynamic = 'force-dynamic'
 export const fetchCache = "force-no-store";
 
+export const runtime = 'nodejs'
+
 const handler = NextAuth(getAuthOptions())
 
 export { handler as GET, handler as POST }
